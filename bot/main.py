b4bbc574c0bc4893beb576c43288bd6987a27487
LCarpenter87 <L.carpenter@outlook.com>
2022-04-09 01:46:39 +0100

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -6,6 +6,9 @@ from discord.ext import commands
 DATABASE_URL = os.getenv('DATABASE_URL')
 bot = commands.Bot(command_prefix="!")
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+cursor = conn.cursor() 
+conn.autocommit = True 
+
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 
@@ -14,24 +17,34 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 async def on_ready():
     print(f"Logged in as {bot.user.name}({bot.user.id})")
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send("pong")
 
+async def validate_code(code, author):
+    ## for now just checking that the code is still un-redeemed 
+    ## to do is add the concept of many users being able to redeem a code one time. 
+    
+    cursor.execute(f"SELECT code from master_codes left outer join redemptions r using (code) where r.code IS NULL and code = '{code}'")
+    rows = cursor.fetchall() 
+    return len(rows) > 0
+ 
+async def create_redemption(code, author):
+    cursor.execute("INSERT INTO redemptions values (code, author) ")
+    
 @bot.command()
 async def redeem(ctx, code):
     author  = ctx.author
-    valid_codes = ['alphabet', 'bubbles', 'cat']
     
     disallowed_users = ['HaroldSaxon', 'Macrologia']
     if ctx.author.name in disallowed_users:
         await ctx.reply(f'Sorry. {author} is not allowed to enter')
         return None
     
-    if code in valid_codes:
-        await ctx.reply(f"{ctx.author.name} You redeemed: {code}")
-    else: 
-        await ctx.send('Invalid, sorry')
+    if validate_code(code, author):
+        create_redemption(code, author)
+        
+        await ctx.reply('Cool. Redeemed')
+    else:
+        await ctx.reply("Sorry, I couldn't redeem that")
+        
     
 if __name__ == "__main__":
     bot.run(TOKEN)
